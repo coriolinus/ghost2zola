cf363da0314bfdf2728fe8eab99b920bde2d0c26
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-10-23 03:19:01 +0200

    implement prefix search and single-db search

--- src/bin/find_ghost_db.rs
+++ src/bin/find_ghost_db.rs
@@ -1,4 +1,4 @@
-use ghost2zola::{find_ghost_db, try_archive};
+use ghost2zola::{find_ghost_db, find_ghost_dbs, try_archive};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -7,13 +7,26 @@ struct Opt {
     /// Path to check
     #[structopt(parse(from_os_str))]
     path: PathBuf,
+
+    /// Prefix to search for db within
+    #[structopt(parse(from_os_str), long)]
+    prefix: Option<PathBuf>,
+
+    /// Find all possible DB paths instead of searching for a single one
+    #[structopt(long)]
+    all: bool,
 }
 
 fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::from_args();
     let mut archive = try_archive(&opt.path)?;
-    for db_path in find_ghost_db(&mut archive)? {
-        println!("{}", db_path.display());
+    if opt.all {
+        for db_path in find_ghost_dbs(&mut archive)? {
+            println!("{}", db_path.display());
+        }
+    } else {
+        let db_path = find_ghost_db(&mut archive, opt.prefix)?;
+        println!("found db path: {}", db_path.display());
     }
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -27,12 +27,17 @@ impl FileType {
 pub enum Error {
     #[error("IO")]
     Io(#[from] std::io::Error),
-    #[error("Input does not appear to be a (compressed) tar file")]
+    #[error("input does not appear to be a (compressed) tar file")]
     NotTar,
+    #[error("input does not contain a ghost.db within search area")]
+    GhostDbNotFound,
+    #[error("input contains more than one ghost.db within search area")]
+    MultipleGhostDb,
 }
 
 fn try_to_tar_reader(path: &Path) -> Result<Box<dyn Read>, Error> {
     let reader = std::fs::File::open(&path)?;
+    let reader = std::io::BufReader::new(reader);
     match FileType::try_from_path(&path) {
         Some(FileType::Tar) => Ok(Box::new(reader)),
         Some(FileType::TarGz) => {
@@ -52,7 +57,7 @@ pub fn try_archive(path: &Path) -> Result<tar::Archive<Box<dyn Read>>, Error> {
     Ok(tar::Archive::new(reader))
 }
 
-pub fn find_ghost_db<'a, R>(
+pub fn find_ghost_dbs<'a, R>(
     archive: &'a mut tar::Archive<R>,
 ) -> Result<impl 'a + Iterator<Item = PathBuf>, Error>
 where
@@ -70,4 +75,35 @@ where
             })
             .flatten()
     }))
+}
+
+fn conditional_filter<'a>(
+    iter: impl 'a + Iterator<Item = PathBuf>,
+    prefix: Option<PathBuf>,
+) -> Box<dyn 'a + Iterator<Item = PathBuf>> {
+    match prefix {
+        None => Box::new(iter),
+        Some(prefix) => {
+            let filter = move |path: &PathBuf| path.starts_with(prefix.as_path());
+            Box::new(iter.filter(filter))
+        }
+    }
+}
+
+pub fn find_ghost_db<R>(
+    archive: &mut tar::Archive<R>,
+    prefix: Option<PathBuf>,
+) -> Result<PathBuf, Error>
+where
+    R: Read,
+{
+    let db_iter = find_ghost_dbs(archive)?;
+    let db_iter = conditional_filter(db_iter, prefix);
+    let mut dbs: Vec<_> = db_iter.take(2).collect();
+    match dbs.len() {
+        0 => Err(Error::GhostDbNotFound),
+        2 => Err(Error::MultipleGhostDb),
+        1 => Ok(std::mem::take(&mut dbs[0])),
+        _ => unreachable!(),
+    }
 }
